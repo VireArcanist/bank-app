c2413ecb5abe96da55ee3b682f1897b153f9347b
VireArcanist <virelyn@outlook.de>
2026-05-27 12:05:38 +0200

    Add Bank class with account management

---
+++ Bank.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class Bank {
+    HashMap<String, Account> accounts = new HashMap<>();
+
+    public void addAccount(Account account) {
+        accounts.put(account.iban, account);
+    }
+
+    public void showAll() {
+        for (Account account : accounts.values()) {
+            System.out.println(account.iban + " | " + account.owner + " | " + account.balance);
+        }
+    }
+    public Account getAccount(String iban) {
+        return accounts.get(iban);
+    }
+
+}
+
+
